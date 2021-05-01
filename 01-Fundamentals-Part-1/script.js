8c9ed3d7017b553fdc35acce3fb8e2681d555b58
gsharma101 <sharmag8121@gmail.com>
2021-05-01 04:57:27 -0700

    Initial

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,37 @@
+/*
+let js = "amazing";
+if(js === 'amazing') alert('Javascript is FUN!');
+const data = 48 + 8 + 23 - 10
+console.log(data);
+*/
+//*Math Operator
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now -2018;
+console.log(ageJonas,ageSarah); //?multiple console.log
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); //!Different Values of console.log
+// ! 2**3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Gaurav";
+const lastName = "Sharma";
+console.log(firstName+" "+lastName);
+//* Assignment operators
+let x = 10 + 5;
+x += 10;// x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+*/
+/*
+//? Marks and Johns BMI
+const marksWeight = 78;
+const johnWeight = 92;
+const markHeight = 1.67;
+const johnHeight = 1.88;
+const markBMI = marksWeight / markHeight ** 2;
+const johnBMI = johnWeight / markHeight ** 2;
+console.log(markBMI, johnBMI);
+*/
