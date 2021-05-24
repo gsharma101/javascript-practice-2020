faaa9800d5169869a1dfec30952071face989444
gsharma101 <sharmag8121@gmail.com>
2021-05-24 05:59:25 -0700

    initial commit

--- 03-Guess-My-Number/starter/script.js
+++ 03-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent = '🎉 Correct Number');
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = "⛔No Number";
+    }
+});
