abe1270cd4ba146aab7722f81ddc452e58ec158e
gsharma101 <sharmag8121@gmail.com>
2021-05-26 02:08:22 -0700

    updaing modal class

--- 04-Modal/starter/script.js
+++ 04-Modal/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+btnCloseModel.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
