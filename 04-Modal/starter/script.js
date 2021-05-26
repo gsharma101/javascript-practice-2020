346bfe94412692f297dc46984e1e329efb3652f3
gsharma101 <sharmag8121@gmail.com>
2021-05-26 03:03:55 -0700

    Finishing modal

--- 04-Modal/starter/script.js
+++ 04-Modal/starter/script.js
@@ -4,14 +4,29 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', function () {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+//!opening the modal funtion
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
-btnCloseModel.addEventListener('click', function () {
+//!closing the Modal function
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
+}
+//!forloop for three btns
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+//!clode button
+btnCloseModel.addEventListener('click', closeModal);
+//!overlay close click
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "Escape") {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        };
+    }
 });
