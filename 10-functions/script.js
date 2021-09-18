7fa462ce81b992da02a7bdd973c1d0829774c682
Gaurav Sharma <sharmag8121@gmail.com>
2021-09-18 23:18:14 +0530

    updating challenge 1

--- 10-functions/script.js
+++ 10-functions/script.js
@@ -228,3 +228,28 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
+
+const poll = {
+  question: 'What is your favouririte programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n (Write option number)`
+      )
+    );
+    console.log(answer);
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
+  },
+};
+
+poll.registerNewAnswer();
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
