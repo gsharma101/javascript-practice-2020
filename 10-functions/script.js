e267ba3fdd5e0e426d98fc4897d807ab2915d7e5
Gaurav Sharma <sharmag8121@gmail.com>
2021-09-19 00:43:24 +0530

    Coding Challenge 1 completed

--- 10-functions/script.js
+++ 10-functions/script.js
@@ -232,6 +232,7 @@ GOOD LUCK 😀
 const poll = {
   question: 'What is your favouririte programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0,0,0,0] More in the next sextion in array section 
   answers: new Array(4).fill(0),
   registerNewAnswer() {
     // Get answer
@@ -245,7 +246,16 @@ const poll = {
     typeof answer === 'number' &&
       answer < this.answers.length &&
       this.answers[answer]++;
-    console.log(this.answers);
+      this.displayResults();
+      this.displayResults('string');
+  },
+
+  displayResults(type = 'array'){
+      if(type === 'array'){
+        console.log(this.answers);
+      }else if(type === 'string'){
+        console.log(`Poll results are ${this.answers.join(',')}`);
+      }
   },
 };
 
@@ -253,3 +263,8 @@ poll.registerNewAnswer();
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+  poll.displayResults.call({answers: [5,2,3]}, 'string');
+
+  // [5,2,3]
+  // [1,5,3,9,6,1]
