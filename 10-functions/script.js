8aeedee373f2e4a56c4bc12a05a70859ebb7c057
Gaurav Sharma <sharmag8121@gmail.com>
2021-11-07 23:30:58 +0530

    more about closure

--- 10-functions/script.js
+++ 10-functions/script.js
@@ -305,22 +305,31 @@ console.dir(booker);
 // ! More about clousers
 let f;
 
-const g = function(){
+const g = function () {
   const a = 23;
-  f = function(){
+  f = function () {
     console.log(a * 2);
-  }
+  };
 };
 
-const h = function(){
+const h = function () {
   const b = 777;
-  f = function(){
+  f = function () {
     console.log(b * 2);
-  }
-}
+  };
+};
 g();
 f();
 // re-assigning f function
 h();
 f();
-
+// Example
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds `);
+};
+boardPassengers(180, 3);
