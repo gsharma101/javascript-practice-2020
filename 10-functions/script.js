b618be4b6765c872f9caa78493b2f3acddfe751e
Gaurav Sharma <sharmag8121@gmail.com>
2021-11-08 01:47:55 +0530

    end of more about closres

--- 10-functions/script.js
+++ 10-functions/script.js
@@ -325,11 +325,12 @@ h();
 f();
 // Example
 const boardPassengers = function (n, wait) {
-  const perGroup = n / 3;
+  // const perGroup = n / 3;
   setTimeout(function () {
     console.log(`We are now boarding all ${n} passengers`);
     console.log(`There are 3 groups, each with ${perGroup} passengers`);
   }, wait * 1000);
   console.log(`Will start boarding in ${wait} seconds `);
 };
+const perGroup =  1000;
 boardPassengers(180, 3);
