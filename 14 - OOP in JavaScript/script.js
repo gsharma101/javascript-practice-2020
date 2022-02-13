25bc4b472511800d163bd0dff5f4db13fdf79c5f
gsharma101 <sharmag8121@gmail.com>
2022-02-13 22:44:44 +0530

    constructor and new operator

--- 14 - OOP in JavaScript/script.js
+++ 14 - OOP in JavaScript/script.js
@@ -1,2 +1,24 @@
 'use strict';
 // ! Constructor functions and new operator
+const Person = function(firstName, birthYear){
+    // ? Insstance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    // ! Bad practice of using a method Never create a method inside a constructor function
+    this.calcAge = function(){
+        console.log(2037 - this.birthYear);
+    }
+}
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+// 1. A new empty object is created
+// 2. function is called, this = {}
+// 3. {} Linded to a prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack',1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
