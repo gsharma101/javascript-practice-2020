93af1d2fe398abea1f13e6175d67ddea39249ebc
gsharma101 <sharmag8121@gmail.com>
2022-02-14 23:09:16 +0530

    prototypes

--- 14 - OOP in JavaScript/script.js
+++ 14 - OOP in JavaScript/script.js
@@ -4,10 +4,10 @@ const Person = function(firstName, birthYear){
     // ? Insstance properties
     this.firstName = firstName;
     this.birthYear = birthYear;
-    // ! Bad practice of using a method Never create a method inside a constructor function
-    this.calcAge = function(){
-        console.log(2037 - this.birthYear);
-    }
+    //! Bad practice of using a method Never create a method inside a constructor function
+    // this.calcAge = function(){
+    //     console.log(2037 - this.birthYear);
+    // }
 }
 
 const jonas = new Person('Jonas', 1991);
@@ -22,3 +22,8 @@ const jack = new Person('Jack',1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+// * Prototypes
+Person.prototype.calcAge = function(){
+    console.log(2037 - this.birthYear);
+}
